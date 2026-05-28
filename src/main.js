540b75b84bea6321b23bdcee93dec6773574c9d0
Vitaliy <vitalii.yankovskyi.pp.2022@edu.lpnu.ua>
2026-05-28 10:31:37 -0700

    Fix: remove command injection, trigger pull-model CI

--- src/main.js
+++ src/main.js
@@ -1,22 +1,13 @@
 const http = require('http');
-const { exec } = require('child_process');
 
 const port = 80;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-
-  // Vulnerability: Command Injection via user input
-  const userFile = new URL(req.url, `http://${req.headers.host}`).searchParams.get('file');
-  if (userFile) {
-    exec(`cat ${userFile}`, (err, stdout) => {
-      res.end(stdout || 'File not found\n');
-    });
-  } else {
-    res.end('Hello World from microservice-a!\n');
-  }
+  res.end('Hello World from microservice-a!\n');
 });
 
 server.listen(port, () => {
   console.log(`Server running at port ${port}`);
 });
+// pull-model test 2026-05-28
